7e9e2a5bf77ae26336153f8f30145bf7dd8236af
John Wolfe <johnwolfe@Johns-MacBook-Pro-2.local>
2018-01-08 00:39:00 -0500

    file tree

--- playground/json.js
+++ playground/json.js
@@ -1,5 +1,26 @@
-var obj = {
-  name: 'John'
+const fs = require('fs');
+
+var originalNote = {
+    title: 'Some title',
+    body: 'Some body'
 };
 
-var stringObj = JSON.stringify(obj);
+var originalNoteString = JSON.stringify(originalNote);
+
+
+fs.writeFile('notes.json', originalNoteString);
+
+var noteString = fs.readFileSync('notes.json');
+var note = JSON.parse(noteString);
+// note
+console.log(typeof note);
+console.log(note.title);
+
+
+
+
+// try {
+//     fs.writeFileSync('notes.json', originalNoteString);
+// } catch (err) {
+//     console.log('Error writing Notes.json:' + err.message)
+// }
